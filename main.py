32fef367e14845a98598c42b93dddf9e9a841c12
Oleksandr Kozlenko <stupidahhhit@gmail.com>
2026-02-03 15:50:26 +0200

    Part 2 done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from csv import DictWriter
 from os import write
 
@@ -6,15 +7,29 @@ with open("config.txt", "r") as file:
     min_score = int(file.read())
 
 with open("students.csv", "r", encoding="utf-8") as file:
-    data = list(csv.DictReader(file, delimiter=','))
+    students = list(csv.DictReader(file, delimiter=','))
 
-for student in data:
+for student in students:
     student["score"] = int(student["score"])
 
-for i in data:
-    if i["score"] < min_score:
-        with open("retest.csv", "w", encoding="utf-8") as file:
-            writer = csv.DictWriter(file, fieldnames=["id","name","surname","score"])
-            writer.writeheader()
-            for student in data:
-                writer.writerow(student)
+retest_list = []
+for student in students:
+    if student["score"] < min_score:
+        retest_list.append(student)
+
+with open("retest.csv", "w", encoding="utf-8") as file:
+    zagolovok = ["id","name","surname","score"]
+    writer = csv.DictWriter(file,fieldnames=zagolovok)
+    writer.writeheader()
+    for student in retest_list:
+        writer.writerow(student)
+
+success_list = []
+
+for student in students:
+    if student["score"] >= min_score:
+        student["passed"] = True
+        success_list.append(student)
+
+with open("best_students.json","w", encoding="utf-8") as file:
+    json.dump(success_list, file, ensure_ascii=False, indent=2)
